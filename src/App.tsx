3dd6bef1b563b9cabe469425ac9943d61048bebe
gui625 <gm59250944@gmail.com>
2025-10-04 02:27:28 -0300

    tela de login

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Results from './pages/Results';
+import Login from './pages/Login';
 import Header from './components/Header';
 import BurnoutQuiz from './components/BurnoutQuiz';
 import './styles/main.css';
@@ -12,6 +13,7 @@ const App: React.FC = () => {
       <div>
         <Header />
         <Switch>
+          <Route path="/login" component={Login} />
           <Route path="/" exact component={Home} />
           <Route path="/quiz" component={BurnoutQuiz} />
           <Route path="/results" component={Results} />
