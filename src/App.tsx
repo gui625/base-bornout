986621ab6ef33a7b6ec8fe63f275627b7ad48285
gui625 <gm59250944@gmail.com>
2025-10-21 01:59:57 -0300

    integração da api de IA

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Results from './pages/Results';
-import Login from './pages/Login';
-import Statistics from './pages/Statistics';
 import Header from './components/Header';
 import BurnoutQuiz from './components/BurnoutQuiz';
 import './styles/main.css';
@@ -14,14 +12,9 @@ const App: React.FC = () => {
       <div>
         <Header />
         <Switch>
-          <Route path="/login" component={Login} />
-          <Route path="/home" component={Home} />
+          <Route path="/" exact component={Home} />
           <Route path="/quiz" component={BurnoutQuiz} />
           <Route path="/results" component={Results} />
-          <Route path="/statistics" component={Statistics} />
-          <Route path="/" exact>
-            <Redirect to="/login" />
-          </Route>
         </Switch>
       </div>
     </Router>
